7ea99d3cee50748d148c793e53f228e0d0753825
xinjian.lxj <xinjian.lxj@ali-070318n.hz.ali.com>
2014-05-12 16:46:38 +0800

    增加ThreadUncaughtExceptionHanlder的测试代码

---
+++ Java/src/seker/threads/ExceptionHandler.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2014 XXX Inc. All rights reserved.
+ */
+package seker.threads;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+/**
+ * @author xinjian.lxj
+ * 
+ */
+public class ExceptionHandler implements UncaughtExceptionHandler {
+    @Override
+    public void uncaughtException(Thread thread, Throwable throwable) {
+        System.out.println("This thread is:" + Thread.currentThread().getName() + ", The exception thread is:" + thread.getName() + ", Message:" + throwable.getMessage());
+    }
+}
---
+++ Java/src/seker/threads/ThreadsTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014 XXX Inc. All rights reserved.
+ */
+package seker.threads;
+
+/**
+ * @author xinjian.lxj
+ *
+ */
+public class ThreadsTest {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler());
+        
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                double i = 12 / 0;// 抛出异常的地方
+                System.out.println(i);
+            }
+        }, "worker thread").start();
+        
+        String[] ary = new String[] {"Hello Exception."};
+        System.out.println(ary[1]);
+    }
+    
+    
+}
